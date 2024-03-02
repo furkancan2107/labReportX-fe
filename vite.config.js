006ff15b1281a490308e873a7f560f45898e4548
Furkan Can <101043567+furkancan2107@users.noreply.github.com>
2024-03-02 23:09:15 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 5173,
+    strictPort : true,
+  },
   server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:5173",
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080', 
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '') 
-      }
+      '/api' : 'https://labreport-furkancan2107-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com'
     }
   }
+})
